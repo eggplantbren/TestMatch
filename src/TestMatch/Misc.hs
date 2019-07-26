aa5346089975e2c4f4d57c1ad8a29ab5237dc09a
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-07-26 13:59:28 +1200

    getting ready for simulation

--- src/TestMatch/Misc.hs
+++ src/TestMatch/Misc.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module TestMatch.Misc where
 
 -- Imports
+import Control.Monad.Primitive
 import qualified Data.Vector.Unboxed as U
+import System.Random.MWC
+import TestMatch.Player
 
 -- Relative probabilities for each possible number of runs.
 -- Maximum should be 1.
@@ -18,4 +23,15 @@ runProbs' = U.map (* invTot) runProbs
 -- and the bowler.
 strikeRate :: Double
 strikeRate = U.sum $ U.zipWith (*) runProbs' (U.fromList [0..6])
+
+-- What possible outcomes are there from a ball of cricket?
+-- For simplicity, start with either a wicket or a number of runs.
+data BallOutcome = Wicket | Runs Int
+
+-- Simulate one ball of cricket and return the result in a PrimMonad
+simulateBall :: PrimMonad m
+             => Player -> Gen (PrimState m) -> m BallOutcome
+simulateBall Player {..} rng = do
+    -- Placeholder
+    return $ Runs 0
 
